cd06de6a7778f3bfea175aae67530c753dcb7a3c
Aretzera <agroup168@gmail.com>
2025-12-25 11:24:49 +0700

    helyeah
    Co-authored-by: rtyuionoop <outofidea@users.noreply.github.com>

--- idk.ino
+++ idk.ino
@@ -1,154 +1,89 @@
-//Servo motor library
 #include <Servo.h>
-//Initialize variables
-int  mode = 0;
-int axe = 0;            
-int buttonState1 = 0;    
-int buttonState2  = 0;   
-int prevButtonState1 = 0;
-int prevButtonState2 = 0; 
- 
-int ldrtopr=  0;                // top-right LDR                          
-int ldrtopl = 1;               // top-left LDR                          
-int ldrbotr = 2;               //  bottom-right LDR                     
-int ldrbotl = 3;               // bottom-left  LDR                   
-int topl = 0;
-int topr = 0; 
-int botl = 0;
-int  botr = 0;
 
-//Declare two servos
-Servo servo_updown;
-Servo servo_rightleft;
+#define LDR_TL_PIN A0
+#define LDR_TR_PIN A1
+#define LDR_BL_PIN A2
+#define LDR_BR_PIN A3
 
-int  threshold_value=10;           //measurement sensitivity
+#define BUT1_pin 4
+#define BUT1_pin 5
+
+#define SERVO_AZIMUTH 9
+#define SERVO_ALTITUDE 10
+
+#define LDR_DEADZONE 40
+
+#define SERVO_CONTROL_INTERVAL_MS 500
+
+int ldr_tl_val;
+int ldr_tr_val;
+int ldr_bl_val;
+int ldr_br_val;
+
+long prev_time;
+
+Servo servo_azi;
+Servo servo_alt;
+
+int servo_azi_val;
+int servo_alt_val;
 
 void setup()
 {
-  Serial.begin(9600);                                //serial connection setup  //opens  serial port, sets data rate to 9600 bps
-  Serial.println("CLEARDATA");                       //clear  all data that’s been place in already
-  Serial.println("LABEL,t,voltage,current,power,Mode");   //define the column headings (PLX-DAQ command)
 
-  pinMode(12, INPUT);              //Mode  switch Button
-  pinMode(11, INPUT);              //Axis switch
-  pinMode(A4,  INPUT);              //Potentiometer for right-left movement and for up-down movement
-  
-  servo_updown.attach(5);             //Servo motor up-down movement
-  servo_rightleft.attach(6);          //Servo motor right-left movement
+  Serial.begin(9600);
+
+  servo_azi.attach(9);
+  servo_alt.attach(10);
 }
 
 void loop()
 {
-//  pv_power();
-char  Mode;
-    float volt = analogRead(A5)*5.0/1023;
-    float voltage = 2*volt;                //  Volt=(R1/R1+R2)*Voltage / R1=R2=10Ohms  => voltage=2*volt)
-    float current = voltage/20;            //  I=voltage/(R1+R2) 
-    float power  = voltage*current;
-    Serial.print("DATA,TIME,"); // PLX-DAQ command
-    Serial.print(voltage);    //send the voltage to serial port
-    Serial.print(",");
-    Serial.print(current);    //send the current to serial port
-    Serial.print(",");
-    Serial.print(power);  //send the power to serial port
-    Serial.print(",");
-    
-//    Serial.println(Mode);      
-  buttonState1 = digitalRead(12);
-  if (buttonState1 != prevButtonState1)  {
-    if (buttonState1 == HIGH) {
-      //Change mode and ligh up the correct  indicator  
-      if (mode == 1) {
-        mode = 0;
-      } else {
-        mode = 1;
-      }
+
+  unsigned long time = millis();
+
+  ldr_tl_val = analogRead(A0);
+  ldr_tr_val = analogRead(A1);
+  ldr_bl_val = analogRead(A2);
+  ldr_br_val = analogRead(A3);
+
+  int ldr_avg_t = (ldr_tl_val + ldr_tr_val) / 2;
+  int ldr_avg_b = (ldr_bl_val + ldr_br_val) / 2;
+  int ldr_avg_r = (ldr_tr_val + ldr_br_val) / 2;
+  int ldr_avg_l = (ldr_tl_val + ldr_bl_val) / 2;
+
+  int ldr_avg = (ldr_tl_val + ldr_tr_val + ldr_bl_val + ldr_br_val) / 4;
+
+  int diff_azi = ldr_avg_r - ldr_avg_l;
+  int diff_alt = ldr_avg_t - ldr_avg_b;
+
+  if (abs(diff_azi) > LDR_DEADZONE)
+  {
+    if (diff_azi < 0)
+    {
+      servo_azi_val -= 1; servo_azi_val = constrain(servo_azi_val, 0, 180);
+    }
+    else
+    {
+      servo_azi_val += 1;
+      servo_azi_val = constrain(servo_azi_val, 0, 180);
     }
   }
-  prevButtonState1 = buttonState1;
-  delay(50); // Wait for 50 millisecond(s)
-  
-  if (mode == 0) {
-    Mode='M';
-    Serial.println(Mode);   //send Mode "Manual" to serial port    
-    manualsolartracker();
-  } else { // mode automatic
-    Mode = 'A';
-    Serial.println(Mode);      
-    automaticsolartracker(); //send Mode "Automatic" to serial port
-    } 
-}
 
-void  automaticsolartracker(){
-  
-     //capturing analog values of each LDR
-     topr= analogRead(ldrtopr);         //capturing analog value of top right LDR
-     topl= analogRead(ldrtopl);         //capturing analog value of top left LDR
-     botr= analogRead(ldrbotr);         //capturing analog value of bot right LDR
-     botl= analogRead(ldrbotl);         //capturing analog value of bot left LDR
-
-    // calculating average
-     int avgtop = (topr + topl) / 2;     //average  of top LDRs
-     int avgbot = (botr + botl) / 2;     //average of bottom LDRs
-     int avgleft = (topl + botl) / 2;    //average of left LDRs
-     int avgright  = (topr + botr) / 2;   //average of right LDRs
-   
-    //Get the different  
-     int diffelev = avgtop - avgbot;      //Get the different average betwen  LDRs top and LDRs bot
-     int diffazi = avgright - avgleft;    //Get the different  average betwen LDRs right and LDRs left
-    
-    //left-right movement of  solar tracker
-     
-      if (abs(diffazi) >= threshold_value){        //Change  position only if light difference is bigger then the threshold_value
-       if  (diffazi > 0) {
-        if (servo_rightleft.read() < 180) {
-          servo_rightleft.write((servo_updown.read()  + 2));
-        }
-      }
-      if (diffazi <  0) {
-        if (servo_rightleft.read()  > 0) {
-          servo_rightleft.write((servo_updown.read() - 2));
-        }
-      }
+  if (abs(diff_alt) > LDR_DEADZONE)
+  {
+    if (diff_alt < 0)
+    {
+      servo_alt_val -= 1; servo_alt_val = constrain(servo_alt_val, 0, 180);
     }
-             
-      //up-down movement of solar tracker
-
-      if (abs(diffelev) >= threshold_value){    //Change position only if light  difference is bigger then thethreshold_value
-       if (diffelev > 0) {
-        if  (servo_updown.read() < 180) {
-          servo_updown.write((servo_rightleft.read()  - 2));
-        }
-      }
-      if (diffelev <  0) {
-        if (servo_updown.read()  > 0) {
-          servo_updown.write((servo_rightleft.read() + 2));
-        }
-      }
-    }       
- }  
-
-void manualsolartracker(){
-  buttonState2  = digitalRead(13);
-  if (buttonState2 != prevButtonState2) {
-    if (buttonState2  == HIGH) {
-      //Change mode and ligh up the correct indicator  
-      if  (axe == 1) {
-        axe = 0;
-      } else {
-        axe = 1;
-      }
+    else
+    {
+      servo_alt_val += 1;
+      servo_alt_val = constrain(servo_alt_val, 0, 180);
     }
   }
-  prevButtonState2 = buttonState2;
-  delay(50); // Wait for 50  millisecond(s)
-  if (axe == 0) {     //control right-left movement
-    servo_rightleft.write(map(analogRead(A4),  0, 1023, 0, 180));
-  } else { // //control up-down movement
-    servo_updown.write(map(analogRead(A4),  0, 1023, 0, 180));
-  } 
+
+  servo_azi.write(servo_azi_val);
+  servo_alt.write(servo_alt_val);
+
 }
-
-
-
-
